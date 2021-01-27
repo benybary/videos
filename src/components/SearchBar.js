475b3acb99f52f5f69a10d9e65e2830e09931ab3
benybary <benybary@gmail.com>
2021-01-27 11:14:48 +0200

    refactor to functional components and use hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component{
-state = { term: '' }
-
-onInputChange = (event) => {
-    this.setState(
-        { term: event.target.value}
-    )
-}
-
-onFormSubmit = (event) => {
-    event.preventDefault();
-    
-    this.props.onFormSubmit(this.state.term);
-
-}
+const SearchBar = ({ onFormSubmit }) => {
+	const [term, setTerm] = useState("");
 
 
-    render() {
-        return(
-            <div className="search-bar ui segment">
-             <form onSubmit={this.onFormSubmit} className="ui form">
-                 <label>Video Search</label>
-                 <input type="text" 
-                 value={this.state.term} 
-                 onChange={this.onInputChange}
-                 />
-             </form>
-            </div>
-        );
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		onFormSubmit(term);
     };
+    return (
+        <div className="search-bar ui segment">
+            <form onSubmit={onSubmit} className="ui form">
+                <label>Video Search</label>
+                <input
+                    type="text"
+                    value={term}
+                    onChange={(event) => setTerm(event.target.value)}
+                />
+            </form>
+        </div>
+    );
 };
+
 
 export default SearchBar;
